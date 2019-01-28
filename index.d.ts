b7a7cdd1082c46edb0635a0f29e7339afa5c096b
Brendon Sled <brendon@tinyroar.com>
2019-01-28 14:51:20 -0600

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,12 @@
+
 declare module 'mui-table' {
   import React from 'react';
+  import { TableBodyProps } from '@material-ui/core/TableBody';
   import { TablePaginationProps } from '@material-ui/core/TablePagination';
   import { TableRowProps } from '@material-ui/core/TableRow';
   import { TableCellProps } from '@material-ui/core/TableCell';
 
-  export interface ColumnDef<T> {
+  export interface ColumnDef<T = any> {
     name: string;
     header?: string | React.ReactNode;
     cell?: (data: T) => React.ReactNode;
@@ -17,14 +19,21 @@ declare module 'mui-table' {
     columns?: ColumnDef<T>[];
   }
 
-  export interface MuiTableProps<T> {
+  export interface MuiTableProps<T = any> {
     data: T[];
+    bodyProps?: TableBodyProps;
+    containerProps?: any;
     includeHeaders?: boolean;
     headerProps?: TableCellProps;
-    rowProps?: TableRowProps;
+    rowProps?:
+      | TableRowProps
+      | ((obj: { rowData: T }) => TableRowProps | void);
     bodyRowProps?:
       | TableRowProps
       | ((obj: { rowData: T }) => TableRowProps | void);
+    bodyCellProps?:
+      | TableCellProps
+      | ((obj: { rowData: T }) => TableCellProps | void);
     cellProps?:
       | TableCellProps
       | ((obj: { rowData: T }) => TableCellProps | void);
@@ -36,6 +45,15 @@ declare module 'mui-table' {
     pagination?: {};
     addPlaceholderRows?: boolean;
     tableWrapperProps?: React.HTMLAttributes<HTMLDivElement>;
+    headerCellProps?:
+      | TableCellProps
+      | ((obj: { rowData: T }) => TableCellProps | void);
+    headerRowProps?:
+      | TableRowProps
+      | ((obj: { rowData: T }) => TableRowProps | void);
+    isCellHovered?: (obj: { column: string; rowData: T; hoveredColumn: string; hoveredRowData: T; }) => void;
+    isCellSelected?: (obj: { column: string; rowData: T; }) => void;
+    classes?: { container?: string; tableWrapper?: string; cellHovered?: string; cellSelected?: string; };
   }
   export default class MuiTable<T> extends React.Component<MuiTableProps<T>> {}
 }
